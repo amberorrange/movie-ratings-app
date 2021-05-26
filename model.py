aef066b770457a3b2125cf4014053f043e075378
amberorrange <orrangeamber@gmail.com>
2021-05-26 22:03:10 +0000

    completed part 1 of lab

--- model.py
+++ model.py
@@ -19,6 +19,63 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     print('Connected to the db!')
 
 
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    overview = db.Column(db.Text())
+    release_date = db.Column(db.DateTime())
+    poster_path = db.Column(db.String(50), nullable=False)
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'   
+
+
+
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+    
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'   
+
+
+
 if __name__ == '__main__':
     from server import app
 

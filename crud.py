677da2d6fda59f4afd48b6457b5a5994af37429d
amberorrange <orrangeamber@gmail.com>
2021-05-27 17:06:21 +0000

    added crud.py

---
+++ crud.py
@@ -0,0 +1,18 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
